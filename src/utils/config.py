031f69ee44a60578d4f646be2032b9dc45ad73bb
nk-randers <nivethan.kandiah@randers.dk>
2024-10-09 16:07:37 +0200

    update config

--- src/utils/config.py
+++ src/utils/config.py
@@ -10,6 +10,6 @@ DEBUG = os.getenv('DEBUG', 'False') in ['True', 'true']
 PORT = os.getenv('PORT', '8080')
 POD_NAME = os.getenv('POD_NAME', 'pod_name_not_set')
 
-KEYCLOAK_URL = os.environ["KEYCLOAK_URL"].strip()
-KEYCLOAK_REALM = os.environ["KEYCLOAK_REALM"].strip()
-KEYCLOAK_CLIENT_ID = os.environ["KEYCLOAK_CLIENT_ID"].strip()
+KEYCLOAK_URL = os.environ["KEYCLOAK_URL"].rstrip()
+KEYCLOAK_REALM = os.environ["KEYCLOAK_REALM"].rstrip()
+KEYCLOAK_CLIENT_ID = os.environ["KEYCLOAK_CLIENT_ID"].rstrip()
